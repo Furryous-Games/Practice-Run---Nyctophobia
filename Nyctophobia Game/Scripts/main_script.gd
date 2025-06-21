dd10726798432dd3c1e7cea624d1f5ee4a285dfb
TekBot0507 <144193352+TekBot0507@users.noreply.github.com>
2025-06-21 04:06:13 -0400

    Developed Door System

--- Nyctophobia Game/Scripts/main_script.gd
+++ Nyctophobia Game/Scripts/main_script.gd
@@ -1,9 +1,12 @@
 extends Node
 
 @onready var room_tilemap: TileMap = $"../Tile Sets/Room Tilemap"
+@onready var player: Node2D = $"../Player"
+@onready var camera: Camera2D = $"../Camera"
 
 # Master grid for information about the house
 var house_grid = []
+var curr_room = Vector2(0, 0)
 
 # Holds information about the size of the house in "rooms x rooms"
 const house_size = 2
@@ -12,16 +15,25 @@ const house_size = 2
 const room_size_x = 11
 const room_size_y = 9
 
+# Holds information for moving the camera around
+const original_camera_pos = Vector2(110, 90)
+
 # Defines what each atlas coord is in terms of building type
 const building_type_by_atlas_coords = {
 	"wall": [Vector2i(0, 1), Vector2i(1, 1), Vector2i(4, 1), Vector2i(0, 2), Vector2i(4, 2), Vector2i(0, 5), Vector2i(1, 5), Vector2i(4, 5)],
 	"floor": [Vector2i(1, 2), Vector2i(2, 2), Vector2i(3, 2), Vector2i(1, 3), Vector2i(2, 3), Vector2i(3, 3), Vector2i(1, 4), Vector2i(2, 4), Vector2i(3, 4)],
 	"window": [Vector2i(2, 1), Vector2i(0, 3), Vector2i(4, 3), Vector2i(2, 5)],
-	"door": [Vector2i(3, 1), Vector2i(0, 4), Vector2i(4, 4), Vector2i(3, 5)]
+	"door_n": [Vector2i(3, 1)], 
+	"door_w": [Vector2i(0, 4)], 
+	"door_e": [Vector2i(4, 4)], 
+	"door_s": [Vector2i(3, 5)]
 	}
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
+	# Provides a pointer to the main script for the player
+	player.main_script = self
+	
 	# Sets up the house grid to be a square 2D list
 	for y in range(house_size):
 		house_grid.append([])
@@ -69,6 +81,60 @@ func _ready() -> void:
 					
 					# Sets the found tile type in the house grid
 					house_grid[house_y][house_x][room_y][room_x]["type"] = tile_type
+
+
+func move_to_room(new_room, door_entered) -> void:
+	# Checks if the door is valid and unlocked
+	if (
+		# Checks that the expected room is within bounds
+		new_room.x >= 0 and new_room.y >= 0 and new_room.x < house_size and new_room.y < house_size
+		):
+		
+		# Defines what door for the program to search for when determining where to place the player in the new room
+		var new_door_direction = (
+			"door_n" if door_entered == "door_s" else 
+			"door_e" if door_entered == "door_w" else 
+			"door_s" if door_entered == "door_n" else 
+			"door_w"
+		)
+		
+		# Gets the location of the determined door in the new room
+		var new_room_door_location = get_door_location_in_room(new_room, new_door_direction)
+		
+		# Defines the offset from the door depending on what type it is
+		var spawn_tile = new_room_door_location + ( 
+			Vector2(0, -1) if door_entered == "door_n" else
+			Vector2(1, 0) if door_entered == "door_e" else
+			Vector2(0, 1) if door_entered == "door_s" else
+			Vector2(-1, 0)
+			)
+		
+		# Updates the player's position
+		player.player_pos = spawn_tile
+		# Stop any ongoing movements
+		player.tween_pos.stop()
+		# Move the player's visual to the new location
+		player.position = (spawn_tile + Vector2(new_room[0] * 12, new_room[1] * 10)) * 20
+		
+		# Update the camera's position to the new room
+		camera.position = original_camera_pos + Vector2(new_room[0] * 12 * 20, new_room[1] * 10 * 20)
+		
+		# Update the current room to the new room
+		curr_room = new_room
+		
+	else:
+		print("The door is locked!")
+
+func get_door_location_in_room(room_pos, door_direction) -> Vector2:
+	var new_room_grid = house_grid[room_pos.y][room_pos.x]
+	
+	for room_y in range(len(new_room_grid)):
+		for room_x in range(len(new_room_grid[room_y])):
+			if new_room_grid[room_y][room_x]["type"] == door_direction:
+				return Vector2(room_x, room_y)
+	
+	return Vector2(-1, -1)
+
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
--- Nyctophobia Game/scripts/player.gd
+++ Nyctophobia Game/scripts/player.gd
@@ -1,57 +1,119 @@
 extends Node2D
 
+var main_script = null
+
+@onready var player_sprite: AnimatedSprite2D = $"Player Sprite"
+@onready var walk_cooldown: Timer = $"Walk Cooldown"
+
 # Stores the player's position in relation to the tiles; "5, 4" is the center of a 11x9 room
-var player_pos: Array[int] = [5, 4]
+var player_pos = Vector2(5, 4)
 # For tweening (allows for smooth movement)
 var tween_pos: Tween
-
 
 func _ready() -> void:
 	self.position = Vector2(player_pos[0] * 20, player_pos[1] * 20)
 
-
 # Whether or not a move command is currently being enacted
 var executing := false
 # To store the next input given while the movement animation is playing
-var move_cue := []
+var next_movement = null
 
 
 # Handles user inputs
 func _input(event: InputEvent) -> void:
-	# return if not an accepted input; allows one additional input to be cued
-	if (
-		not event is InputEventKey
-		or event.as_text() not in ["Up", "Down", "Right", "Left"]
-		or event.is_released()
-		or move_cue.size() > 1
-	):
-		return
-	# Stores the input as text ("Up", "Down", "Right", "Left")
-	var key: String = event.as_text()
-	# Stores the next input to be played once the current ends
-	move_cue.append([1 if key == "Up" or key == "Down" else 0, 1 if key == "Down" or key == "Right" else -1, key])
-	
-	# If animation is currently playing (move being performed): return
-	if executing:
-		return
-	executing = true
-	
-	# Move player position; animates the movent
-	# NOTE: Waiting until the full movement ends before accepting another input felt unresponsive. \
-		# This remedies that, though it also doesn't feel quite right
-	# Performs the cued movement inputs
-	while move_cue.size() != 0:
-		player_pos[move_cue[0][0]] += move_cue[0][1]
+	if event.is_pressed():
+		# Handles events for when a directional key is pressed
+		if event.as_text() in ["Up", "Down", "Right", "Left", "W", "A", "S", "D"]:
+			# return if not an accepted input; allows one additional input to be cued
+			if (
+				not event is InputEventKey
+				or next_movement != null
+			):
+				return
+			
+			# Stores the input as text ("Up", "Down", "Right", "Left")
+			var key: String = event.as_text()
+			
+			# Converts WASD keys into directional keys
+			if key in ["W", "A", "S", "D"]:
+				key = "Up" if key == "W" else "Right" if key == "D" else "Down" if key == "S" else "Left"
+			
+			var expected_movement = [
+				Vector2(0 if not key in ["Left", "Right"] else 1 if key == "Right" else -1,
+				0 if not key in ["Up", "Down"] else 1 if key == "Down" else -1),
+				key
+				]
+			
+			# Checks to ensure that the expected tile is clear
+			var expected_tile_pos = player_pos + expected_movement[0]
+			var expected_tile_metadata = main_script.house_grid[main_script.curr_room[1]][main_script.curr_room[0]][expected_tile_pos[1]][expected_tile_pos[0]]
+			
+			if expected_tile_metadata["type"] != "floor" or expected_tile_metadata["object"] != null:
+				return
+			
+			# Stores the next input to be played once the current ends
+			next_movement = expected_movement
+			
+			# If animation is currently playing (move being performed): return
+			if executing:
+				return
+			executing = true
+			
+			# Move player position; animates the movent
+			# NOTE: Waiting until the full movement ends before accepting another input felt unresponsive. \
+				# This remedies that, though it also doesn't feel quite right
+			# Performs the cued movement inputs
+			move_player()
+		
+		# Handles events for when an interact key is pressed
+		if event.as_text() in ["E", "Enter"]:
+			var expected_tile_pos
+			var expected_tile_metadata
+			
+			# Checks the surrounding tiles for interactable objects
+			for check_tile_pos in [Vector2(0, -1), Vector2(1, 0), Vector2(0, 1), Vector2(-1, 0)]:
+				expected_tile_pos = player_pos + check_tile_pos
+				expected_tile_metadata = main_script.house_grid[main_script.curr_room[1]][main_script.curr_room[0]][expected_tile_pos[1]][expected_tile_pos[0]]
+				
+				var door_room_change = {
+					"door_n": Vector2(0, -1),
+					"door_e": Vector2(1, 0),
+					"door_s": Vector2(0, 1),
+					"door_w": Vector2(-1, 0),
+				}
+				
+				# Checks if the object is a door
+				if expected_tile_metadata["type"] in door_room_change.keys():
+					var expected_room = main_script.curr_room + door_room_change[expected_tile_metadata["type"]]
+					print("There is a door here!")
+					main_script.move_to_room(expected_room, expected_tile_metadata["type"])
+					break
+
+func move_player() -> void:
+	# Checks if there is a cued movement
+	if next_movement != null:
+		# Updates the player's current position
+		player_pos += next_movement[0]
+		
+		# Moves the player's visual smoothly along the planned path
 		tween_pos = create_tween().set_ease(Tween.EASE_OUT).set_trans(Tween.TRANS_SINE)
-		tween_pos.tween_property(self, "position", Vector2(player_pos[0] * 20, player_pos[1] * 20), 0.2)
+		tween_pos.tween_property(self, "position", Vector2(player_pos[0] + (main_script.curr_room[0] * 12), player_pos[1] + (main_script.curr_room[1] * 10)) * 20, 0.2)
+		
 		# Sets the animation according to the direction of the input
-		match move_cue[0][2]:
-			"Up": $AnimatableBody2D/Sprite2D.animation = "walk_up"
-			"Down": $AnimatableBody2D/Sprite2D.animation = "walk_down"
-			"Left": $AnimatableBody2D/Sprite2D.animation = "walk_left"
-			"Right": $AnimatableBody2D/Sprite2D.animation = "walk_right"
-		$AnimatableBody2D/Sprite2D.frame = 1
-		move_cue.remove_at(0)
-		await get_tree().create_timer(0.2).timeout
-		$AnimatableBody2D/Sprite2D.frame = 0
+		match next_movement[1]:
+			"Up": player_sprite.animation = "walk_up"
+			"Down": player_sprite.animation = "walk_down"
+			"Left": player_sprite.animation = "walk_left"
+			"Right": player_sprite.animation = "walk_right"
+		player_sprite.frame = 1
+		
+		# Clears the player's next movement
+		next_movement = null
+		walk_cooldown.start()
+
+func _on_walk_cooldown_timeout() -> void:
+	player_sprite.frame = 0
+	
 	executing = false
+	
+	move_player()
