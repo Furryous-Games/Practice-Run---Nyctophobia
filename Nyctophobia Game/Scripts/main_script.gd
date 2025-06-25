b8decedbe3a8c07bbb4efe1b0e3788159cceecce
Bryceicle <64384892+Bryceicle@users.noreply.github.com>
2025-06-25 00:47:21 -0400

    Room variants (#5)
    * Developed Variants for Rooms
    
    * snakecase baby
    
    ---------
    
    Co-authored-by: TekBot0507 <144193352+TekBot0507@users.noreply.github.com>

--- Nyctophobia Game/Scripts/main_script.gd
+++ Nyctophobia Game/Scripts/main_script.gd
@@ -45,7 +45,8 @@ const FURNITURE_TYPE_BY_ATLAS_COORDS = {
 const WALKABLE_OBJECTS := ["book_shelf"]
 
 # Holds information about the size of the house in "rooms x rooms"
-const HOUSE_SIZE: int = 2
+
+const HOUSE_SIZE = Vector2i(4, 3)
 
 # Holds information about the size of each room on the tilemap
 const ROOM_SIZE_X: int = 11
@@ -65,6 +66,7 @@ var house_grid: Array = []
 var curr_room := Vector2i(0, 0)
 
 # Holds information regarding the default room lighting
+
 var room_lighting: int = 6 # 6
 var window_emission: int = 1 # 1
 var lamp_emission: int = 2 # 2
@@ -101,14 +103,14 @@ var completed_tasks: Array
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	# Sets up the house grid to be a square 2D list
-	for y in range(HOUSE_SIZE):
+	for y in range(HOUSE_SIZE.y):
 		house_grid.append([])
-		for x in range(HOUSE_SIZE):
+		for x in range(HOUSE_SIZE.x):
 			house_grid[-1].append([])
 	
 	# Iterates through each room in the house
-	for room_y in range(HOUSE_SIZE):
-		for room_x in range(HOUSE_SIZE):
+	for room_y in range(HOUSE_SIZE.y):
+		for room_x in range(HOUSE_SIZE.x):
 			# Creates 2D lists for each tile in the room
 			for y in range(ROOM_SIZE_Y):
 				house_grid[room_y][room_x].append([])
@@ -126,8 +128,8 @@ func _ready() -> void:
 	var atlas_coords
 	
 	# Loops through all the rooms inside of the house
-	for house_y in range(HOUSE_SIZE):
-		for house_x in range(HOUSE_SIZE):
+	for house_y in range(HOUSE_SIZE.y):
+		for house_x in range(HOUSE_SIZE.x):
 			# Loops through all the tiles in the room
 			for room_y in range(ROOM_SIZE_Y):
 				for room_x in range(ROOM_SIZE_X):
@@ -252,8 +254,8 @@ func move_to_room(new_room: Vector2i, door_entered: String) -> bool:
 			# Checks that the expected room is within bounds
 			new_room.x >= 0 
 			and new_room.y >= 0 
-			and new_room.x < HOUSE_SIZE 
-			and new_room.y < HOUSE_SIZE
+			and new_room.x < HOUSE_SIZE.x
+			and new_room.y < HOUSE_SIZE.y
 	):
 		
 		# Defines what door for the program to search for when determining where to place the player in the new room
